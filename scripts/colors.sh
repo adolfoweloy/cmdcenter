8add7092436e3231771c556ccdb072faa1b1c1b8
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-05-04 13:06:34 +1000

    Change from textual to non-ascii block graphic character

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -3,11 +3,11 @@
 colors() {
   # if present first arg sets the number of columns
   # otherwise, the default number of columns will be 2
-  columns=${1:-"2"}
+  columns=${1:-"5"}
 
   # print out the color palette
   for i in `seq 255`; do
-    printf "\033[38;5;%dmColor %03d\t" $i $i
+    printf "\033[38;5;%dm\u2588\u2588 %03d\t" $i $i
     if (( $i % $columns == 0 )); then
       echo;
     fi;
